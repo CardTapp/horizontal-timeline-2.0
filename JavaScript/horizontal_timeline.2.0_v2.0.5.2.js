bdf868a5670154d0373db6d5763877a1ef6adabb
Sascha Mayr <mayr.sascha@gmail.com>
2020-08-27 13:53:46 +0200

    Fix DE4577

--- JavaScript/horizontal_timeline.2.0_v2.0.5.2.js
+++ JavaScript/horizontal_timeline.2.0_v2.0.5.2.js
@@ -1642,7 +1642,7 @@ Docs at http://horizontal-timeline.ycodetech.co.uk
 				// Get wrapper width
 				wrapperWidth = timelineComponents['timelineWrapper'].width(),
 				// Get the css left value of the last event date, remove the px unit and add 100 to it.
-				lastEventLeft = Number(timelineComponents['timelineEvents'].last().css('left').replace('px', '')) + 100;
+				lastEventLeft = Number(timelineComponents['timelineEvents'].last().css('left').replace('px', '')) + 140;
 
 			// Set a fail-safe, if lastEventLeft is less than the wrapperWidth then use the wrapperWidth as totalWidth.
 			// Stops the timeline width from being too small.
